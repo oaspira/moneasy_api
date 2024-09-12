8c6bf370f547926f22d8d0c3f00d4c96fc8dad8a
Samira Costa <samiramanuelac@gmail.com>
2024-09-12 23:52:15 +0200

    feat: added rack-cors and configure initializers cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*' # Aceitar requisições de qualquer origem. Você pode definir domínios específicos como 'https://meusite.com'
+      
+      resource '*',
+        headers: :any, # Permite qualquer cabeçalho
+        methods: [:get, :post, :put, :patch, :delete, :options, :head], # Métodos HTTP permitidos
+        credentials: false # Definir como true se precisar permitir cookies e credenciais
+    end
+  end
+  
