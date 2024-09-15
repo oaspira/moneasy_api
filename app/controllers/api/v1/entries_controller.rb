ec136021df3c616ab5ecc2002119d12fde0f1f50
Luiz Muniz <luiz.ppiruk@gmail.com>
2024-09-15 14:18:37 +0100

    added reference year filter

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::EntriesController < ApplicationController
   before_action :set_entry, only: %i[show update destroy]
   before_action :set_monthly_budget, only: %i[index]
   before_action :set_entry_status, only: %i[index]
+  before_action :set_yearly_budget, only: %i[index]
 
   def index
     # authorize Entry
@@ -9,6 +10,7 @@ class Api::V1::EntriesController < ApplicationController
     @entries = Entry.all
     @entries = @entries.for_monthly_budget(@monthly_budget) if params[:reference_month].present?
     @entries = @entries.for_entry_status(@entry_status) if params[:key].present?
+    @entries = @entries.for_yearly_budget(@yearly_budget) if params[:reference_year].present?
 
     @entries
   end
@@ -76,6 +78,10 @@ class Api::V1::EntriesController < ApplicationController
 
   def set_monthly_budget
     @monthly_budget = MonthlyBudget.find_by(month: params[:reference_month])
+  end
+
+  def set_yearly_budget
+    @yearly_budget = YearlyBudget.find_by(year: params[:reference_year])
   end
 
   def set_entry_status
